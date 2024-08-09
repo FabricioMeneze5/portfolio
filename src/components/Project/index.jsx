d17f0aad740dcd6342298cf2cbe599c44d102ce2
FabricioMeneze5 <fabricio-menezes@live.com>
2024-08-09 05:29:05 +0100

    ADD section Projects

---
+++ src/components/Project/index.jsx
@@ -0,0 +1,13 @@
+import Paragraph from '../Paragraph'
+import Tittle from '../Tittle'
+import { Card, LinkButton } from './style'
+
+const Project = () => (
+  <Card>
+    <Tittle>Projects</Tittle>
+    <Paragraph color="secondary">Tasks with VueJS</Paragraph>
+    <LinkButton>View</LinkButton>
+  </Card>
+)
+
+export default Project
--- src/containers/Projects/index.tsx
+++ src/containers/Projects/index.tsx
@@ -1,8 +1,37 @@
+import Project from '../../components/Project'
 import Tittle from '../../components/Tittle'
+
+import { List } from './slyles'
 
 const Projects = () => (
   <section>
     <Tittle fontSize={16}>Projects</Tittle>
+    <List>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+      <li>
+        <Project />
+      </li>
+    </List>
   </section>
 )
 
